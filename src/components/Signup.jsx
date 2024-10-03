06788b4d858af25f1d70f3133a878f825c3a05d6
Muhammad Arslan <arslan05342@gmail.com>
2024-10-03 13:02:23 +0500

    Addition/soe buit-in validations and password vaidations

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
+
 export default function Signup() {
+  const[passwordDoesNotMatch, setPasswordDoesNotMatch] = useState(false)
+
   function handleSubmit(event) {
     event.preventDefault();
+    setPasswordDoesNotMatch(false);
 
     const formData = new FormData(event.target); // giving by browser
     // const email = formData.get('email'); // it will be too heavy to hande a values 
@@ -8,6 +13,12 @@ export default function Signup() {
     console.log(acquisitionChannel)
     const data = Object.fromEntries(formData.entries()); // it is also provided by browser it will convert formData entries to key pair vvalues. but we cant get values of mutiple radio buttons values as have mutiple names 
     data.acquisition = acquisitionChannel;
+    if(data.password !== data['confirm-password']){
+      setPasswordDoesNotMatch(true);
+      // formData.reset();
+      return;
+    }
+
     console.log(data);
   }
 
@@ -18,13 +29,13 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" />
+        <input id="email" type="email" name="email" required/>
       </div>
 
       <div className="control-row">
         <div className="control">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" minLength={6} required/>
         </div>
 
         <div className="control">
@@ -33,7 +44,10 @@ export default function Signup() {
             id="confirm-password"
             type="password"
             name="confirm-password"
+            minLength={6}
+            required
           />
+          <div className="control-error">{passwordDoesNotMatch && <p>Password must match</p>}</div>
         </div>
       </div>
 
@@ -42,18 +56,18 @@ export default function Signup() {
       <div className="control-row">
         <div className="control">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" name="first-name" />
+          <input type="text" id="first-name" name="first-name" required/>
         </div>
 
         <div className="control">
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" name="last-name" />
+          <input type="text" id="last-name" name="last-name" required/>
         </div>
       </div>
 
       <div className="control">
         <label htmlFor="phone">What best describes your role?</label>
-        <select id="role" name="role">
+        <select id="role" name="role" required>
           <option value="student">Student</option>
           <option value="teacher">Teacher</option>
           <option value="employee">Employee</option>
@@ -92,7 +106,7 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="terms-and-conditions">
-          <input type="checkbox" id="terms-and-conditions" name="terms" />I
+          <input type="checkbox" id="terms-and-conditions" name="terms" required/>I
           agree to the terms and conditions
         </label>
       </div>
